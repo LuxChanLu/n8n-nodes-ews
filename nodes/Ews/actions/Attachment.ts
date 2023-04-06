264228b7fe2029b384bdf66dca2f23053aee7b62
Hugo Meyronneinc <hugomeyronneinc@gmail.com>
2023-04-06 18:39:30 +0200

    fix

--- nodes/Ews/actions/Attachment.ts
+++ nodes/Ews/actions/Attachment.ts
@@ -4,22 +4,22 @@ import { IExecuteFunctions, INodeExecutionData } from "n8n-workflow"
 export async function get(func: IExecuteFunctions, exchange: ExchangeService, idx: number, returnData: INodeExecutionData[]): Promise<any> {
 	const attachmentId = func.getNodeParameter('attachmentId', idx) as string
 	const items = await exchange.GetAttachments([attachmentId], BodyType.Text, [])
-	const attachement = items.Responses.at(0)?.Attachment as FileAttachment
-	if (attachement) {
+	const attachment = items.Responses.at(0)?.Attachment as FileAttachment
+	if (attachment) {
 		returnData.push({
 			json: {
-				id: attachement.Id,
-				name: attachement.Name,
-				contentType: attachement.ContentType,
-				contentId: attachement.ContentId,
-				contentLocation: attachement.ContentLocation,
-				size: attachement.Size,
-				lastModifiedTime: attachement.LastModifiedTime,
-				isInline: attachement.IsInline,
-				fileName: attachement.FileName,
-				isContactPhoto: attachement.IsContactPhoto,
+				id: attachment.Id,
+				name: attachment.Name,
+				contentType: attachment.ContentType,
+				contentId: attachment.ContentId,
+				contentLocation: attachment.ContentLocation,
+				size: attachment.Size,
+				lastModifiedTime: attachment.LastModifiedTime,
+				isInline: attachment.IsInline,
+				fileName: attachment.FileName,
+				isContactPhoto: attachment.IsContactPhoto,
 			},
-			binary: { attachement: await func.helpers.prepareBinaryData(Buffer.from(attachement.Base64Content, 'base64'), attachement.FileName), },
+			binary: { attachment: await func.helpers.prepareBinaryData(Buffer.from(attachment.Base64Content, 'base64'), attachment.FileName), },
 			pairedItem: idx
 		})
 	}
